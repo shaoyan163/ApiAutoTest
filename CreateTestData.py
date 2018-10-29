0697ac23ca44b4bafbb554872f1f488982007cf8
unknown <hzsyy@hzdomain1.com>
2018-10-29 17:14:53 +0800

    提交

--- CreateTestData.py
+++ CreateTestData.py
@@ -5,27 +5,22 @@ from selenium import webdriver
 
 class BrowserTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        pass
 
     def test_baidu(self):
-        self.driver.get("http://www.baidu.com")
-        tx = self.driver.find_element_by_id("su")
-        self.assertEqual(u"百度一下", tx.get_attribute("value"))
+        self.assertEqual(5, 3+2)
 
     def test_sougou(self):
-        self.driver.get("https://www.sogou.com/")
-        self.assertIn(u"关于搜狗", self.driver.page_source)
+        self.assertEqual(6, 3 + 3)
 
     def test_souhu(self):
-        self.driver.get("http://www.sohu.com/")
-        self.assertIn(u"邮箱", self.driver.page_source)
+        self.assertEqual(7, 3 + 4)
 
     def tearDown(self):
-        self.driver.quit()
+        pass
 
 
 if __name__ == "__main__":
     unittest.main()
-
 
 
